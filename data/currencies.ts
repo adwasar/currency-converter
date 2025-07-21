ee4f1fcc01a747694164e269786d108f47674a30
vladislav <adwasarik@gmail.com>
2025-07-21 16:40:32 +0300

    chores: add data of currencies

---
+++ data/currencies.ts
@@ -0,0 +1,56 @@
+const currencyItems = [
+  {
+    img: require('@/assets/images/currencies/AED.png'),
+    title: 'AED',
+  },
+  {
+    img: require('@/assets/images/currencies/AUD.png'),
+    title: 'AUD',
+  },
+  {
+    img: require('@/assets/images/currencies/CAD.png'),
+    title: 'CAD',
+  },
+  {
+    img: require('@/assets/images/currencies/CHF.png'),
+    title: 'CHF',
+  },
+  {
+    img: require('@/assets/images/currencies/EUR.png'),
+    title: 'EUR',
+  },
+  {
+    img: require('@/assets/images/currencies/GBP.png'),
+    title: 'GBP',
+  },
+  {
+    img: require('@/assets/images/currencies/ILS.png'),
+    title: 'ILS',
+  },
+  {
+    img: require('@/assets/images/currencies/INR.png'),
+    title: 'INR',
+  },
+  {
+    img: require('@/assets/images/currencies/JPY.png'),
+    title: 'JPY',
+  },
+  {
+    img: require('@/assets/images/currencies/MXN.png'),
+    title: 'MXN',
+  },
+  {
+    img: require('@/assets/images/currencies/MYR.png'),
+    title: 'MYR',
+  },
+  {
+    img: require('@/assets/images/currencies/NGN.png'),
+    title: 'NGN',
+  },
+  {
+    img: require('@/assets/images/currencies/USD.png'),
+    title: 'USD',
+  },
+]
+
+export default currencyItems
