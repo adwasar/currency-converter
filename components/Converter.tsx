a88650eeb6d33036642539e1d556da7d69e22e39
vladislav <adwasarik@gmail.com>
2026-02-19 12:34:27 +0200

    fix: spit input value states for other ConverterSection section

--- components/Converter.tsx
+++ components/Converter.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
-import AppText from './AppText';
-import CurrencyPicker from './CurrencyPicker';
+import ConverterSection from './ConverterSection';
 
 interface Props {
   currencySelected: string | null;
@@ -10,55 +9,19 @@ interface Props {
 }
 
 export default function Converter({ currencySelected, handleOpenCurrencyPicker }: Props) {
-  const [amountText, setAmountText] = useState<string>('1.00');
-
-  const handleChange = (text: string) => {
-    let cleaned = text.replace(/[^0-9.]/g, '');
-
-    const parts = cleaned.split('.');
-    if (parts.length > 2) {
-      cleaned = parts[0] + '.' + parts.slice(1).join('');
-    }
-
-    if (parts.length === 2) {
-      const integer = parts[0];
-      const decimal = parts[1].slice(0, 2);
-      cleaned = integer + (decimal ? '.' + decimal : '.');
-    }
-
-    setAmountText(cleaned);
-  };
-
-  const handleBlur = () => {
-    if (!amountText.trim()) {
-      setAmountText('0.00');
-      return;
-    }
-
-    const num = Number(amountText);
-    if (!isNaN(num)) {
-      setAmountText(num.toFixed(2));
-    } else {
-      setAmountText('0.00');
-    }
-  };
-
   return (
     <View style={styles.converter}>
-      <View>
-        <AppText style={styles.text}>Amount</AppText>
-        <View style={styles.currencyRow}>
-          <CurrencyPicker currencySelected={currencySelected} openCurrencyBottomSheet={handleOpenCurrencyPicker} />
-          <TextInput
-            style={styles.input}
-            value={amountText}
-            keyboardType="numeric"
-            onChangeText={handleChange}
-            onBlur={handleBlur}
-          />
-        </View>
-      </View>
-      <View></View>
+      <ConverterSection
+        title="Amount"
+        currencySelected={currencySelected}
+        handleOpenCurrencyPicker={handleOpenCurrencyPicker}
+      />
+      <View style={styles.splitter}></View>
+      <ConverterSection
+        title="Converted Amount"
+        currencySelected={currencySelected}
+        handleOpenCurrencyPicker={handleOpenCurrencyPicker}
+      />
     </View>
   );
 }
@@ -68,26 +31,11 @@ const styles = StyleSheet.create({
     padding: 24,
     backgroundColor: '#fff',
     borderRadius: 18,
+    gap: 24,
   },
-  currencyRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  input: {
-    paddingVertical: 10,
-    paddingHorizontal: 14,
-    height: 38,
-    width: 125,
-    fontSize: 18,
-    textAlign: 'right',
-    borderRadius: 5,
-    backgroundColor: '#EFEFEF',
-    color: '#3C3C3C',
-    fontFamily: 'SangBleuSans-Medium',
-  },
-  text: {
-    color: '#808080',
-    fontSize: 14,
+  splitter: {
+    width: '100%',
+    height: 1,
+    backgroundColor: '#E7E7EE',
   },
 });
---
+++ components/ConverterSection.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+
+import AppText from './AppText';
+import CurrencyPicker from './CurrencyPicker';
+
+interface Props {
+  currencySelected: string | null;
+  handleOpenCurrencyPicker: () => void;
+  title: string;
+}
+
+export default function ConverterSection({ currencySelected, handleOpenCurrencyPicker, title }: Props) {
+  const [amountText, setAmountText] = useState<string>('1.00');
+
+  const handleChange = (text: string) => {
+    let cleaned = text.replace(/[^0-9.]/g, '');
+
+    const parts = cleaned.split('.');
+    if (parts.length > 2) {
+      cleaned = parts[0] + '.' + parts.slice(1).join('');
+    }
+
+    if (parts.length === 2) {
+      const integer = parts[0];
+      const decimal = parts[1].slice(0, 2);
+      cleaned = integer + (decimal ? '.' + decimal : '.');
+    }
+
+    setAmountText(cleaned);
+  };
+
+  const handleBlur = () => {
+    if (!amountText.trim()) {
+      setAmountText('0.00');
+      return;
+    }
+
+    const num = Number(amountText);
+    if (!isNaN(num)) {
+      setAmountText(num.toFixed(2));
+    } else {
+      setAmountText('0.00');
+    }
+  };
+
+  return (
+    <View>
+      <AppText style={styles.text}>{title}</AppText>
+      <View style={styles.sectionRow}>
+        <CurrencyPicker currencySelected={currencySelected} openCurrencyBottomSheet={handleOpenCurrencyPicker} />
+        <TextInput
+          style={styles.input}
+          value={amountText}
+          keyboardType="numeric"
+          onChangeText={handleChange}
+          onBlur={handleBlur}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 10,
+    paddingHorizontal: 14,
+    height: 38,
+    width: 125,
+    fontSize: 18,
+    textAlign: 'right',
+    borderRadius: 5,
+    backgroundColor: '#EFEFEF',
+    color: '#3C3C3C',
+    fontFamily: 'SangBleuSans-Medium',
+  },
+  text: {
+    color: '#808080',
+    fontSize: 14,
+  },
+});
