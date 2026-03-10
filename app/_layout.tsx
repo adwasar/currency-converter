621371d8137c7c343b9d33092b192eef4d62c5cc
vladislav <adwasarik@gmail.com>
2026-03-10 13:03:05 +0200

    feat: add reserve endpoints for rate

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout() {
     'SFPro-Medium': require('@/assets/fonts/sf-pro-display-medium.otf'),
   });
 
-  // Connecting fonts
   useEffect(() => {
     if (loaded || error) {
       SplashScreen.hideAsync();
@@ -40,30 +39,38 @@ export default function RootLayout() {
     const target = targetCurrency.title.toLowerCase();
     const amount = Number(baseCurrency.amount);
 
+    const urls = [
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${base}.min.json`,
+      `https://latest.currency-api.pages.dev/v1/currencies/${base}.min.json`,
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${base}.json`,
+      `https://latest.currency-api.pages.dev/v1/currencies/${base}.json`,
+    ];
+
     const timer = setTimeout(async () => {
-      try {
-        const res = await fetch(`https://currency-api.pages.dev/v1/currencies/${base}.json`);
-        const data = await res.json();
+      for (let url of urls) {
+        try {
+          const res = await fetch(url);
+          if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+          const data = await res.json();
 
-        const rate = data[base][target];
-        const calculated = (rate * amount).toFixed(2);
+          const rate = data[base]?.[target];
+          if (rate == null) throw new Error(`Rate not found for ${target}`);
 
-        setTargetCurrency((prev) => ({
-          ...prev,
-          amount: calculated,
-        }));
-      } catch (err) {
-        console.error(err);
+          const calculated = (rate * amount).toFixed(2);
+          setTargetCurrency((prev) => ({
+            ...prev,
+            amount: calculated,
+          }));
+          return;
+        } catch (err) {
+          console.warn(`Failed to fetch from ${url}:`, err);
+        }
       }
+      console.error('All currency API requests failed');
     }, 400);
 
     return () => clearTimeout(timer);
   }, [baseCurrency.title, baseCurrency.amount, targetCurrency.title]);
-
-  if (!loaded && !error) {
-    return null;
-  }
-  // END Connecting fonts
 
   return (
     <CurrencyContext value={currencyContextValue}>
